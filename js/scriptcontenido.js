23abbf2c27f2346dd6576ccbc73f7c8f9afab149
Silvia <162silvia@gmail.com>
2021-02-04 05:41:30 +0100

    galcambios

--- js/scriptcontenido.js
+++ js/scriptcontenido.js
@@ -1,13 +1,17 @@
 const KEY = "945da0b433d1c512e2248fae93c2da9e";
 const IMAGE = "https://image.tmdb.org/t/p/w500";
 
+const CLIENTID= "9bd2530ef98e5812cba0e70ac384ede44dab7390175a077d865c3f2d580f30b0";
+const POSTER = "https://simkl.net/posters/";
+const POSTER2 = "_c.jpg";
+
 let info= document.getElementById("info");
 
 //Funcion que me coge el parametro del GET URL
 const getParameterByName = (name, url) => {
     if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
         results = regex.exec(url);
     if (!results) return null;
     if (!results[2]) return '';
@@ -123,4 +127,72 @@ const opiniones = () => {
     // })//fin fetch
 }
 
-// document.addEventListener("DOMContentLoaded", opiniones);
+//document.addEventListener("DOMContentLoaded", opiniones); 
+
+
+
+
+
+//Relacion de dos apis por genero
+
+// let arrayimagenes = document.querySelectorAll(".images__container");
+
+
+fetch("https://api.themoviedb.org/3/movie/"+id+"?api_key="+KEY)  
+  .then(datos => datos.json())   
+   .then(datos_json => {
+
+    //Saco el primer genero
+    let genero = datos_json.genres[0].name;
+    console.log(genero);
+
+fetch("https://api.simkl.com/tv/genres/"+genero+"?client_id="+CLIENTID)
+  .then(datos => datos.json())   
+   .then(datos_json => {
+
+    console.log(datos_json);
+    let arrayrutas = [];
+    //me saco 6
+    for(let i=0; i<6; i++){
+        //guardo las 6 rutas en un array
+
+        //console.log(POSTER+datos_json[i].poster+POSTER2);
+        arrayrutas[i] = POSTER+datos_json[i].poster+POSTER2;
+        
+         
+    }
+
+    console.log(arrayrutas);
+   
+    //Recorro la clase y le asigno su src por posiciones del array
+
+    // NO LO MUESTRA???
+//     for(let j=0; j<arrayimagenes.length; j++){
+//         //como son del mismo tamaño igualo
+//              arrayimagenes[j].setAttribute("src", arrayrutas[j]);
+//              console.log(arrayimagenes[j].getAttribute("src"));
+//   }
+
+//CAMBIAR ESTO -- PRUEBA
+let im1 =document.getElementById("im1");
+let im2 =document.getElementById("im2");
+let im3 =document.getElementById("im3");
+let im4 =document.getElementById("im4");
+let im5 =document.getElementById("im5");
+let im6 =document.getElementById("im6");
+
+
+im1.src=arrayrutas[0];
+im2.src=arrayrutas[1];
+im3.src=arrayrutas[2];
+im4.src=arrayrutas[3];
+im5.src=arrayrutas[4];
+im6.src=arrayrutas[5];
+
+
+})//fin segundo fetch
+
+
+   })//fin primer fetch
+
+
