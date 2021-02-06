323a045f282c764c546e26a33e41ca9eec606354
Silvia <162silvia@gmail.com>
2021-02-06 21:11:04 +0100

    arregloerrorescss

--- js/scriptcontenido.js
+++ js/scriptcontenido.js
@@ -19,27 +19,28 @@ const getParameterByName = (name, url) => {
 }
 
 let id = getParameterByName('data');
-  console.log(id);
-const datosunapelicula = () => {   
+  //console.log(id);
 
-
-
-  //ruta de cada id
-  //https://api.themoviedb.org/3/movie/"+identificador+"?api_key="+KEY;
-  fetch("https://api.themoviedb.org/3/movie/"+id+"?api_key="+KEY+"&language=es")  
-  .then(datos => datos.json())   
-   .then(datos_json => {
-   
-    console.log(datos_json); 
+//Funcion que me da los datos de la pelicula seleccionada
+const datosunapelicula = () => { 
+    //ruta de cada id 
+    fetch("https://api.themoviedb.org/3/movie/"+id+"?api_key="+KEY+"&language=es")  
+    .then(datos => datos.json())   
+    .then(datos_json => {   
+    //console.log(datos_json); 
     let caja = document.createElement("DIV");
     caja.classList.add("info__box2");
-
-    //algunas pueden que no tengan!!
+   
     let imagen = document.createElement("IMG");
-    let ima = datos_json.backdrop_path;
-    imagen.src=IMAGE+ima;  
-    imagen.classList.add("ajustar2");
-
+    //Caso de que la pelicula no tenga imagen, le pongo una por defecto y añado su titulo
+    if(datos_json.backdrop_path==null){
+        imagen.src = "image/b.jpg"; 
+            
+    }else{
+        let ima = datos_json.backdrop_path;
+        imagen.src=IMAGE+ima;              
+    }
+    imagen.classList.add("ajustar2"); 
     caja.appendChild(imagen);
     
     let caja2 = document.createElement("DIV");
@@ -69,80 +70,54 @@ const datosunapelicula = () => {
     caja3.appendChild(puntuacion);
 
     caja2.appendChild(caja3);
-
-    // let texto = document.createElement("SPAN");
-    // texto.textContent = "Generos:";
-    // caja2.appendChild(texto);
-  
+   
     const fragment = document.createDocumentFragment();
-     let generos = document.createElement("DIV");
+    let generos = document.createElement("DIV");
     generos.classList.add("info__box--textge")
                     for (let i = 0; i < datos_json.genres.length; i++) {
                         let generosconte = document.createElement("SPAN");
                         generosconte.innerText= datos_json.genres[i].name;
-
-
                         fragment.appendChild(generosconte);   
                     }
         generos.appendChild(fragment);
         caja2.appendChild(generos);     
-         info.appendChild(caja);
-         info.appendChild(caja2);
-})//fin fetch
-
-}
-
+        info.appendChild(caja);
+        info.appendChild(caja2);
+    })//fin fetch
+}//datosunapelicula
 
 document.addEventListener("DOMContentLoaded", datosunapelicula);
 
 
-//  let opinionn = document.getElementById("opinionn");
-// const opiniones = () => {   
-    // fetch("https://api.themoviedb.org/3/review/22?api_key="+KEY+"&language=es")  
-   
+//Funcion que me da las opiniones de una pelicula 
+//const mostraropiniones = () => { 
     fetch("https://api.themoviedb.org/3/movie/"+id+"/reviews?api_key="+KEY)
     .then(datos => datos.json())   
      .then(datos_json => {
-
-        // let opinion = document.getElementById("opi");
-        // opinion.textContent="fdsdf";
-        // console.log(datos_json.results[0].author);
         console.log(datos_json);
-
-        // let nom1 = document.getElementById("nom1");
-        // nom1.textContent=datos_json.results[0].author
-
-        // let opi1 = document.getElementById("opi1");
         
-        // opi1.textContent=datos_json.results[0].content.substring(0,350);
-      
-        // let opi2 = document.getElementById("opi2");
-        // opi2.textContent=datos_json.results[1].content.substring(0,350);
+        let arti = document.getElementById("arti");        
+        if(datos_json.results.length==0){
+            // console.log("No hay ninguna opinion");
+            let parrafo = document.createElement("P");
+            parrafo.textContent = "No hay ninguna opinion";
+            parrafo.classList.add("cajaarticulos__p");
+            arti.appendChild(parrafo);
 
-        // let opi3 = document.getElementById("opi3");
-        // opi3.textContent=datos_json.results[2].content.substring(0,350);
 
-        // let opi4 = document.getElementById("opi4");
-        // opi4.textContent=datos_json.results[3].content.substring(0,350);
-        let arti = document.getElementById("arti");
-
-        
-        const fragment = document.createDocumentFragment();
-        for(let i=0; i<datos_json.results.length; i++){
-
+        }else{
+            //caso de que si haya opiniones
+        const fragment = document.createDocumentFragment();   
+        //Como máximo voy a mostrar solo 5 opiniones 
+        if(datos_json.results.length>3){    
+        for(let i=0; i<5; i++){
             let articulo = document.createElement("ARTICLE");
             articulo.classList.add("articlecontainer");
 
             let cabeceraarti = document.createElement("HEADER");
             cabeceraarti.classList.add("articlecontainer__header");
             cabeceraarti.textContent = datos_json.results[i].author;
-
-            // let avatar = document.createElement("IMG");
-            // let ima =  datos_json.results[i].author_details.avatar_path; 
-            // avatar.src=IMAGE+ima;  
-           
-            // cabeceraarti.appendChild(avatar);
-
+            
             articulo.appendChild(cabeceraarti);
 
             let parrafo = document.createElement("P");
@@ -151,81 +126,86 @@ document.addEventListener("DOMContentLoaded", datosunapelicula);
 
             articulo.appendChild(parrafo)
             fragment.appendChild(articulo);
-        }
+        }//for
         arti.appendChild(fragment);
+    }//caso de que haya mas de 5 opiniones
+    else{
+        //caso de que haya menos de 5 opiniones
+        for(let i=0; i<datos_json.results.length; i++){
+            let articulo = document.createElement("ARTICLE");
+            articulo.classList.add("articlecontainer");
 
+            let cabeceraarti = document.createElement("HEADER");
+            cabeceraarti.classList.add("articlecontainer__header");
+            cabeceraarti.textContent = datos_json.results[i].author;
+            
+            articulo.appendChild(cabeceraarti);
+
+            let parrafo = document.createElement("P");
+            parrafo.classList.add("articlecontainer__text");
+            parrafo.textContent = datos_json.results[i].content.substring(0,350);
+
+            articulo.appendChild(parrafo)
+            fragment.appendChild(articulo);
+        }//for
+        arti.appendChild(fragment);
+    }
+        }//else
     })//fin fetch
 // }
 
-//document.addEventListener("DOMContentLoaded", opiniones); 
-
-
-
-
 
 //Relacion de dos apis por genero
+//Funcion con la que relaciono 2 apis, obtengo el genero de la pelicula con una api y con la otra saco series de ese genero
 
-// let arrayimagenes = document.querySelectorAll(".images__container");
-
+//const dosapis = () => {
 
 fetch("https://api.themoviedb.org/3/movie/"+id+"?api_key="+KEY)  
   .then(datos => datos.json())   
    .then(datos_json => {
-
-    //Saco el primer genero
-    let genero = datos_json.genres[0].name;
-    console.log(genero);
-
-fetch("https://api.simkl.com/tv/genres/"+genero+"?client_id="+CLIENTID)
-  .then(datos => datos.json())   
-   .then(datos_json => {
-
-    console.log(datos_json);
-    let arrayrutas = [];
-    //me saco 6
+       console.log(datos_json);  
+       let genero; 
+    if(datos_json.genres.length==0){
+        console.log("nulo");
+        //pongo un genero por defecto
+         genero = "comedy";
+    }else{
+       genero = datos_json.genres[0].name;
+    }
+     //Saco el primer genero--Si la pelicula tiene varios generos, me quedo solo con el primero    
+    //console.log("el genero es: "+genero);
+    fetch("https://api.simkl.com/tv/genres/"+genero+"?client_id="+CLIENTID)
+    .then(datos => datos.json())   
+    .then(datos_json => {
+        //console.log(datos_json);
+       let arrayrutas = [];
+        //me saco 8
     for(let i=0; i<8; i++){
         //guardo las 8 rutas en un array
-
         //console.log(POSTER+datos_json[i].poster+POSTER2);
         arrayrutas[i] = POSTER+datos_json[i].poster+POSTER2;
-        
-         
     }
-
-    console.log(arrayrutas);
+    //console.log(arrayrutas);
    
-    //Recorro la clase y le asigno su src por posiciones del array
+    let im1 =document.getElementById("im1");
+    let im2 =document.getElementById("im2");
+    let im3 =document.getElementById("im3");
+    let im4 =document.getElementById("im4");
+    let im5 =document.getElementById("im5");
+    let im6 =document.getElementById("im6");
+    let im7 =document.getElementById("im7");
+    let im8 =document.getElementById("im8");
 
-    // NO LO MUESTRA???
-//     for(let j=0; j<arrayimagenes.length; j++){
-//         //como son del mismo tamaño igualo
-//              arrayimagenes[j].setAttribute("src", arrayrutas[j]);
-//              console.log(arrayimagenes[j].getAttribute("src"));
-//   }
-
-//CAMBIAR ESTO -- PRUEBA
-let im1 =document.getElementById("im1");
-let im2 =document.getElementById("im2");
-let im3 =document.getElementById("im3");
-let im4 =document.getElementById("im4");
-let im5 =document.getElementById("im5");
-let im6 =document.getElementById("im6");
-let im7 =document.getElementById("im7");
-let im8 =document.getElementById("im8");
-
-
-im1.src=arrayrutas[0];
-im2.src=arrayrutas[1];
-im3.src=arrayrutas[2];
-im4.src=arrayrutas[3];
-im5.src=arrayrutas[4];
-im6.src=arrayrutas[5];
-im7.src=arrayrutas[6];
-im8.src=arrayrutas[7];
-
+    im1.src=arrayrutas[0];
+    im2.src=arrayrutas[1];
+    im3.src=arrayrutas[2];
+    im4.src=arrayrutas[3];
+    im5.src=arrayrutas[4];
+    im6.src=arrayrutas[5];
+    im7.src=arrayrutas[6];
+    im8.src=arrayrutas[7];
 })//fin segundo fetch
 
+})//fin primer fetch
 
-   })//fin primer fetch
-
-
+//}
